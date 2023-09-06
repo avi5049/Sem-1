77ea905db0b56ecf1b965eba3592b58e794f213c
avi5049 <140870152+avi5049@users.noreply.github.com>
2023-09-06 20:08:53 +0530

    Add files via upload

---
+++ Addition.c
@@ -0,0 +1,15 @@
+//addition of two numbers
+
+
+#include <stdio.h>
+
+void main (){
+	int a,b,c;
+	printf("Enter integer value a and b:\n");
+	scanf("%d %d",&a,&b);
+	
+	c=a+b;
+	printf("Sum is %d",c);
+
+}
+
