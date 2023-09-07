cdaa038f8d987eaa825b59edc3dae4edfbf1438f
avi5049 <140870152+avi5049@users.noreply.github.com>
2023-09-07 22:05:36 +0530

    Add files via upload

---
+++ Area of circle.c
@@ -0,0 +1,13 @@
+//Q: Print area of cicle
+
+#include <stdio.h>
+
+void main(){
+	float r,A;
+	printf("Enter the Radius of circle:");
+	
+	scanf("%f",&r);
+	A=3.14*r*r;
+	printf("Area of circle is %f",A);
+
+}
