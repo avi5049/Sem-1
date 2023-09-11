b7cea6a41cda01551e708a73b1fec25c6fdda7a2
avi5049 <140870152+avi5049@users.noreply.github.com>
2023-09-11 17:57:25 +0530

    Add files via upload

---
+++ Feet into Inches.c
@@ -0,0 +1,13 @@
+//Q:Print given feet into inches
+
+#include <stdio.h>
+
+void main (){
+	float f,i;
+	printf("Enter feet: ");
+	scanf("%f",&f);
+	i=f*12;
+	printf("Given feet into inches is %f",i);
+	
+}
+
