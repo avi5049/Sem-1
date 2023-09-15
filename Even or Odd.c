6eac792168ace6de31d1e18438e2d63189c40813
avi5049 <140870152+avi5049@users.noreply.github.com>
2023-09-15 19:28:23 +0530

    Add files via upload

---
+++ Even or Odd.c
@@ -0,0 +1,20 @@
+//Q:Check whether number is Even or Odd
+
+
+#include <stdio.h>
+
+void main (){
+	int a;
+	printf("Enter an number: ");
+	scanf("%d",&a);
+	
+	if(a%2==0){
+		printf("Number is even");
+		
+	}
+    else{
+    	printf("Number is odd");
+    	
+	}
+}
+
